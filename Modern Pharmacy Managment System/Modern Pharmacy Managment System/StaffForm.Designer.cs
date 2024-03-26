f12b12aa5af1d68af0f1320af769d30e796cddd9
ishtiak-billah-emon <ishtiakemon27@gmail.com>
2024-03-26 18:07:35 +0600

    Edited staff page

--- Modern Pharmacy Managment System/Modern Pharmacy Managment System/StaffForm.Designer.cs
+++ Modern Pharmacy Managment System/Modern Pharmacy Managment System/StaffForm.Designer.cs
@@ -30,6 +30,7 @@ namespace Modern_Pharmacy_Managment_System
         private void InitializeComponent()
         {
             this.StaffLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // StaffLabel
@@ -37,20 +38,32 @@ namespace Modern_Pharmacy_Managment_System
             this.StaffLabel.AutoSize = true;
             this.StaffLabel.BackColor = System.Drawing.SystemColors.Highlight;
             this.StaffLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StaffLabel.Location = new System.Drawing.Point(401, 277);
+            this.StaffLabel.Location = new System.Drawing.Point(301, 225);
+            this.StaffLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.StaffLabel.Name = "StaffLabel";
-            this.StaffLabel.Size = new System.Drawing.Size(174, 39);
+            this.StaffLabel.Size = new System.Drawing.Size(141, 31);
             this.StaffLabel.TabIndex = 0;
             this.StaffLabel.Text = "Staff Form";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // StaffForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InfoText;
-            this.ClientSize = new System.Drawing.Size(994, 614);
+            this.ClientSize = new System.Drawing.Size(746, 499);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.StaffLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "StaffForm";
             this.Text = "StaffForm";
             this.ResumeLayout(false);
@@ -61,5 +74,6 @@ namespace Modern_Pharmacy_Managment_System
         #endregion
 
         private System.Windows.Forms.Label StaffLabel;
+        private System.Windows.Forms.Button button1;
     }
 }
