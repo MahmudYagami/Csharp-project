52eea12bd07c12b6c90f424de423d3bcb0347abf
Mahmud <131474126+MahmudYagami@users.noreply.github.com>
2024-03-26 18:07:25 +0600

    button

--- Modern Pharmacy Managment System/Modern Pharmacy Managment System/CustomerForm.Designer.cs
+++ Modern Pharmacy Managment System/Modern Pharmacy Managment System/CustomerForm.Designer.cs
@@ -30,26 +30,39 @@ namespace Modern_Pharmacy_Managment_System
         private void InitializeComponent()
         {
             this.CustomerLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // CustomerLabel
             // 
             this.CustomerLabel.AutoSize = true;
             this.CustomerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CustomerLabel.Location = new System.Drawing.Point(368, 274);
+            this.CustomerLabel.Location = new System.Drawing.Point(276, 223);
+            this.CustomerLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.CustomerLabel.Name = "CustomerLabel";
-            this.CustomerLabel.Size = new System.Drawing.Size(252, 39);
+            this.CustomerLabel.Size = new System.Drawing.Size(202, 31);
             this.CustomerLabel.TabIndex = 0;
             this.CustomerLabel.Text = "Customer Form";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // CustomerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(993, 614);
+            this.ClientSize = new System.Drawing.Size(745, 499);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.CustomerLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CustomerForm";
             this.Text = "CustomerForm";
             this.ResumeLayout(false);
@@ -60,5 +73,6 @@ namespace Modern_Pharmacy_Managment_System
         #endregion
 
         private System.Windows.Forms.Label CustomerLabel;
+        private System.Windows.Forms.Button button1;
     }
 }
