02b373460d038a27815f62bca1fad41d2185baa2
ishtiak-billah-emon <ishtiakemon27@gmail.com>
2024-03-26 20:06:57 +0600

    Changes

--- Modern Pharmacy Managment System/Modern Pharmacy Managment System/StaffForm.Designer.cs
+++ Modern Pharmacy Managment System/Modern Pharmacy Managment System/StaffForm.Designer.cs
@@ -40,18 +40,18 @@ namespace Modern_Pharmacy_Managment_System
             this.StaffLabel.AutoSize = true;
             this.StaffLabel.BackColor = System.Drawing.SystemColors.Highlight;
             this.StaffLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StaffLabel.Location = new System.Drawing.Point(401, 277);
+            this.StaffLabel.Location = new System.Drawing.Point(301, 225);
+            this.StaffLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.StaffLabel.Name = "StaffLabel";
-            this.StaffLabel.Size = new System.Drawing.Size(174, 39);
+            this.StaffLabel.Size = new System.Drawing.Size(141, 31);
             this.StaffLabel.TabIndex = 0;
             this.StaffLabel.Text = "Staff Form";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(457, 210);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(455, 171);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -61,9 +61,10 @@ namespace Modern_Pharmacy_Managment_System
             this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
             this.iconButton1.IconColor = System.Drawing.Color.Black;
             this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconButton1.Location = new System.Drawing.Point(638, 132);
+            this.iconButton1.Location = new System.Drawing.Point(343, 101);
+            this.iconButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.iconButton1.Name = "iconButton1";
-            this.iconButton1.Size = new System.Drawing.Size(75, 23);
+            this.iconButton1.Size = new System.Drawing.Size(56, 19);
             this.iconButton1.TabIndex = 2;
             this.iconButton1.Text = "iconButton1";
             this.iconButton1.UseVisualStyleBackColor = true;
@@ -73,25 +74,26 @@ namespace Modern_Pharmacy_Managment_System
             this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.None;
             this.iconButton2.IconColor = System.Drawing.Color.Black;
             this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconButton2.Location = new System.Drawing.Point(470, 320);
+            this.iconButton2.Location = new System.Drawing.Point(399, 137);
+            this.iconButton2.Margin = new System.Windows.Forms.Padding(2);
             this.iconButton2.Name = "iconButton2";
-            this.iconButton2.Size = new System.Drawing.Size(75, 23);
+            this.iconButton2.Size = new System.Drawing.Size(56, 19);
             this.iconButton2.TabIndex = 3;
             this.iconButton2.Text = "iconButton2";
             this.iconButton2.UseVisualStyleBackColor = true;
             // 
             // StaffForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InfoText;
-            this.ClientSize = new System.Drawing.Size(995, 614);
+            this.ClientSize = new System.Drawing.Size(746, 499);
             this.Controls.Add(this.iconButton2);
             this.Controls.Add(this.iconButton1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.StaffLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "StaffForm";
             this.Text = "StaffForm";
             this.ResumeLayout(false);
